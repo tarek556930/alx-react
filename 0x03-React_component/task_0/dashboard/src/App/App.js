f91a965f4d348e356845c3f47a05fff1417965e9
tarek <taga61188@gmail.com>
2024-07-05 15:33:49 +0300

    update

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -8,6 +8,7 @@ import './App.css';
 import PropTypes from 'prop-types';
 import { getLatestNotification } from '../utils/utils';
 
+
 class App extends React.Component {
 	listCourses = [
 		{ id: 1, name: 'ES6', credit: 60 },
